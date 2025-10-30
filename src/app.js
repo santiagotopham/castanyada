1502c64bfa5186e6996767f0b5722776fbc9afc9
Santiago Topham <stopham@practia.uy>
2025-10-30 10:47:01 +0100

    use mysql instead of json

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require("node:path");
 const fs = require("node:fs");
 const crypto = require("node:crypto");
 const methodOverride = require("method-override"); // para "put"
-
+const mysql = require("mysql2/promise");
 
 // Crear la instancia del servidor
 const app = express();
@@ -15,87 +15,131 @@ PORT = process.env.PORT || 6666;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../public")));
+const configConnection = {
+	host: process.env.host,
+	port: process.env.db_port,
+	user: process.env.user,
+	password: process.env.password,
+	database: process.env.database,
+};
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Obtener los datos
-let productes = require("../data/pastisseria.json");
-
 function crearMenu(json, lang) {
-  let tipusProductes = [];
-  let menu = "<ul>";
+	let tipusProductes = [];
+	let menu = "<ul>";
 
-  if (lang === "cat") {
-  json.forEach((producte) => {
-    if (!tipusProductes.includes(producte.menu_name_cat)) {
-      tipusProductes.push(producte.menu_name_cat);
-    }
-  });
-} else {
-  json.forEach((producte) => {
-    if (!tipusProductes.includes(producte.menu_name_esp)) {
-      tipusProductes.push(producte.menu_name_esp);
-    }
-  });
+	if (lang === "cat") {
+		json.forEach((producte) => {
+			if (!tipusProductes.includes(producte.menu_name_cat)) {
+				tipusProductes.push(producte.menu_name_cat);
+			}
+		});
+	} else {
+		json.forEach((producte) => {
+			if (!tipusProductes.includes(producte.menu_name_esp)) {
+				tipusProductes.push(producte.menu_name_esp);
+			}
+		});
+	}
+
+	tipusProductes.forEach((tipus) => {
+		menu += `<li><a href="/${tipus.toLocaleLowerCase()}">${tipus}</a></li>`;
+	});
+	menu += "</ul>";
+	return menu;
 }
-
-  tipusProductes.forEach((tipus) => {
-    menu += `<li><a href="/${tipus.toLocaleLowerCase()}">${tipus}</a></li>`;
-  });
-  menu += "</ul>";
-  return menu
-}
-
-// Realizar el menú
-const menu = crearMenu(productes)
-
-
 
 // Ruta català
-app.get("/", (req, res) => {
-  // Realizar el menú
-  const menu = crearMenu(productes, "cat")
-  res.render("index", { title: "Umm...!",menu, productes, lang: "ESP" });
+app.get("/", async (req, res) => {
+	// Realizar el menú
+	const products = await getProductsFromDb();
+	const menu = crearMenu(products, "cat");
+	res.render("index", {
+		title: "Umm...!",
+		menu,
+		productes: products,
+		lang: "ESP",
+	});
 });
 // Ruta raíz o inicial
-app.get("/esp", (req, res) => {
-  const menu = crearMenu(productes, "esp")
-  res.render("inicio", { title: "Umm...!", menu, productes });
+app.get("/esp", async (req, res) => {
+	const products = await getProductsFromDb();
+	const menu = crearMenu(products, "esp");
+	res.render("inicio", { title: "Umm...!", menu, productes: products });
 });
 
-app.get("/admin", (req, res) => {
-  res.render("admin", { title: "Gestió", menu, productes });
+app.get("/admin", async (req, res) => {
+	const products = await getProductsFromDb();
+	const menu = crearMenu(products, "esp");
+	res.render("admin", { title: "Gestió", menu, productes: products });
 });
 
+// app.post("/insert", (req, res) => {
+// 	const body = req.body;
+// 	body.id = crypto.randomUUID();
+// 	//console.log(body);
+// 	productes.push(body);
+// 	fs.writeFileSync(
+// 		path.join(__dirname, "../data", "pastisseria.json"),
+// 		JSON.stringify(productes, null, 2),
+// 		(err) => {
+// 			if (err) throw err;
+// 		}
+// 	);
+// 	// res.render("admin", { title: "administración", menu, travels });
+// 	res.redirect("/admin");
+// });
 
+app.post("/insert", async (req, res) => {
+	const newProduct = req.body;
 
+	await createProduct(newProduct);
 
-
-app.post("/insert", (req, res) => {
-  const body = req.body;
-  body.id = crypto.randomUUID();
-  //console.log(body);
-  productes.push(body);
-  fs.writeFileSync(
-    path.join(__dirname, "../data", "pastisseria.json"),
-    JSON.stringify(productes, null, 2),
-    (err) => {
-      if (err) throw err;
-    }
-  );
-  // res.render("admin", { title: "administración", menu, travels });
-  res.redirect("/admin")
+	res.redirect("/admin");
 });
 
 // Ruta para manejar errores 404
 app.use((req, res) => {
-  res.render("404", { title: "Error 404", menu });
+	res.render("404", { title: "Error 404", menu });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor arrancado en http://localhost:${PORT}`);
+	console.log(`Servidor arrancado en http://localhost:${PORT}`);
 });
 
+//Gestion de datos
+async function openDbConnection() {
+	return await mysql.createConnection(configConnection);
+}
 
+async function getProductsFromDb() {
+	const connection = await openDbConnection();
+	const query = "SELECT * FROM sweets";
+
+	const [products] = await connection.query(query);
+	await connection.end();
+
+	return products;
+}
+
+async function createProduct(newProduct) {
+	const connection = await openDbConnection();
+	const sql =
+		"INSERT INTO `sweets`(`menu_name_cat`, `name_cat`, `descripcio_cat`, `menu_name_esp`, `name_esp`, `descripcio_esp`, `preu`, `img`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+	const values = [
+		newProduct.menu_name_cat,
+		newProduct.name_cat,
+		newProduct.descripcio_cat,
+		newProduct.menu_name_esp,
+		newProduct.name_esp,
+		newProduct.descripcio_esp,
+		newProduct.preu,
+		newProduct.img,
+	];
+
+	await connection.execute(sql, values);
+	await connection.end();
+}
